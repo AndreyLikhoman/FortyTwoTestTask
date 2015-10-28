3bbea1fd22e1f9a46234f1381833d43b899a8257
Andrey <andrey.likhoman@gmail.com>
2015-10-28 17:56:08 +0200

    change issue in view.py

--- apps/hello/views.py
+++ apps/hello/views.py
@@ -3,6 +3,6 @@ from .models import Users
 
 
 def home(request):
-	user = Users.objects.filter(login = "login")
+	user = Users.objects.filter()
 	return render(request, 'index.html', {'user': user[0]})
 
